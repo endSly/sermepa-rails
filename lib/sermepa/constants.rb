03326d82a52923f03040f2aefedf6b6f862dd4ce
Eric Ponce <tricokun@gmail.com>
2013-07-24 08:53:37 +0200

    Config fix and new transaction type

--- lib/sermepa/constants.rb
+++ lib/sermepa/constants.rb
@@ -1,17 +1,19 @@
 module Sermepa
-  
+
   CURRENCIES = {
     :EUR => '978',
     :USD => '840',
     :GBP => '826',
     :JPY => '392'
   }
-  
+
   TRANSACTION_TYPES = {
     # Payment Requests
     :traditional_payment =>     'A',
     :preauthorization =>        '1',
     :deferred_authorization =>  'O',
+    :authorization =>  '0',
+
     # Recurrent payments
     :recurrent_transaction =>   '5',
     :deferred_initial_recurrent_authorization => 'R',
@@ -24,5 +26,5 @@ module Sermepa
     :deferred_authorization_cancel =>       'Q',
     :deferred_successive_transaction_authorization => 'S'
   }
-  
+
 end
--- lib/sermepa/sermepa.rb
+++ lib/sermepa/sermepa.rb
@@ -4,7 +4,7 @@ require 'sermepa/errors'
 require 'sermepa/signature'
 
 module Sermepa
-  
+
   def build_values(params, transaction_type)
     values = {
       DS_MERCHANT_MERCHANTCODE:     params[:merchant_code] || Sermepa.config.merchant_code,
@@ -15,49 +15,49 @@ module Sermepa
       DS_MERCHANT_TRANSACTIONTYPE:  TRANSACTION_TYPES[transaction_type]
     }
     values[:DS_MERCHANT_MERCHANTURL] = Sermepa.config.merchant_url if Sermepa.config.merchant_url
-    
+
     values
   end
-  
+
   def pay_request(params)
     values = build_values(params, :traditional_payment)
     values[:DS_MERCHANT_PAN] =        params[:pan]
     values[:DS_MERCHANT_CVV2] =       params[:cvv2]
     values[:DS_MERCHANT_EXPIRYDATE] = params[:expiration]
-    
+
     values[:DS_MERCHANT_MERCHANTSIGNATURE] = signature(values)
     call(values)
   end
-  
+
   def confirmation_request(params)
     values = build_values(params, :confirmation)
 
     values[:DS_MERCHANT_MERCHANTSIGNATURE] = signature(values)
     call(values)
   end
-  
+
   def refund_request(params)
     values = build_values(params, :automatic_refund)
 
     values[:DS_MERCHANT_MERCHANTSIGNATURE] = signature(values)
     call(values)
   end
-  
+
   def call(values = {})
     client = Savon.client(config.service_url)
-    
+
     response = client.request :trata_peticion do
       soap.body do |xml|
         request_xml = values.to_xml(root: :DATOSENTRADA, dasherize: false, skip_instruct: true)
         xml.DATOSENTRADA(request_xml)
-    
+
       end
     end
-    
+
     result = Hash.from_xml response.to_hash[:trata_peticion_response][:trata_peticion_return]
     error_code = result['RETORNOXML']['CODIGO'].to_sym
     remote_error = error_code != :'0'
-    
+
     if remote_error
       config._logger.warn("[Sermepa] Remote Error (#{error_code}): #{ERRORS[error_code]}")
       if config.raise_errors
@@ -66,17 +66,17 @@ module Sermepa
         return error_code
       end
     end
-    
+
     response_code = result['RETORNOXML']['OPERACION']['Ds_Response']
-    
-    unless (0..99).include?(response_code.to_i) || response_code.to_i == 900 
+
+    unless (0..99).include?(response_code.to_i) || response_code.to_i == 900
       if config.raise_errors
         raise SermepaPaymentError.new(response_code.to_sym)
       else
         return response_code.to_sym
       end
     end
-    
+
     return false # No errors
   end
 end
